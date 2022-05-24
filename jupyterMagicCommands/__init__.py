604d0f6ff4392833e830236e922a554411db6547
Zhengxiao Wang <zhenwang@microsoft.com>
2022-05-24 22:05:28 +0800

    update clickhouse_ext.py and add pwsh_ext.py

--- jupyterMagicCommands/__init__.py
+++ jupyterMagicCommands/__init__.py
@@ -3,8 +3,9 @@ from . import clickhouse_ext
 from . import plantuml_ext
 from . import html_ext
 from . import writefile_ext
+from . import pwsh_ext
 
 def load_ipython_extension(ipython):
-        for module in [bash_ext, clickhouse_ext, plantuml_ext, html_ext, writefile_ext]:
+        for module in [bash_ext, clickhouse_ext, plantuml_ext, html_ext, writefile_ext, pwsh_ext]:
                 module.load_ipython_extension(ipython)
                 
--- jupyterMagicCommands/clickhouse_ext.py
+++ jupyterMagicCommands/clickhouse_ext.py
@@ -1,4 +1,5 @@
 import argparse
+from pprint import pprint
 import requests
 import pandas as pd
 from io import StringIO
@@ -18,37 +19,47 @@ def ck(line, cell):
     sio = StringIO(cell)
     query = sio.read()
 
-    ret = queryClickHousePlayGround(query, url)
-    if ret.shape[0] == 1 and ret.columns[0] == 'statement':
-        ret = Pretty(ret.iloc[0, 0])
+    ret = queryClickHousePlayGround(query, url, args)
+    if not isinstance(ret, pd.DataFrame):
+        ret = Pretty(ret)
     return ret
 
-def queryClickHousePlayGround(query: str, url: str):
+def queryClickHousePlayGround(query: str, url: str, args: argparse.Namespace):
     data = {
         'query':  query,
         'user': 'play',
         'password': '',
         'default_format': "JSON"
     }
-    r = requests.get(url, params=data)
-    df = None
+    try: 
+        r = requests.get(url, params=data)
+    except Exception as e:
+        print(e)
     if r.ok:
-        df = pd.DataFrame(r.json()['data'])
+        ret = pd.DataFrame(r.json()['data'])
+        if args.plain:
+            if ret.shape != (1, 1):
+                print("--plain only supports results shape = (1, 1). current shape: {}".format(ret.shape))
+                sys.exit(1)
+            ret = str(ret.iloc[0,0])
         print(f"Elapsed: {r.json()['statistics']['elapsed']} sec, read {r.json()['statistics']['rows_read']} rows, {r.json()['statistics']['bytes_read']} bytes.")
     else:
         print("Bad Request: {}".format(r.text))
-    return df
+    for k, v in r.headers.items():
+        print("{}: {}".format(k, v))
+    return ret
 
 def load_ipython_extension(ipython):
     ipython.register_magic_function(ck, 'cell')
 
 def parseDataset(dataset: str) -> str:
     defaultUrl = "https://play.clickhouse.com:443"
-    return {
-        "ontime": "https://gh-api.clickhouse.com/play"
-    }.get(dataset, defaultUrl)
+    return {"ontime": "https://gh-api.clickhouse.com/play"}.get(dataset, defaultUrl)
 
 def getParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', dest='url', type=parseDataset, default=parseDataset('default'))
+    parser.add_argument('--plain', action='store_true', default=False)
     return parser
+
+load_ipython_extension(get_ipython())
