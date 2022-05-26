6deb18c3b4971b6c3e86c9ec2a91d01be237debc
catwang01 <edwardelricwzx@gmail.com>
2022-05-26 21:33:09 +0800

    update clienthouse_ext.py

--- jupyterMagicCommands/clickhouse_ext.py
+++ jupyterMagicCommands/clickhouse_ext.py
@@ -5,10 +5,75 @@ import pandas as pd
 from io import StringIO
 from IPython.display import Pretty
 from IPython.core.magic import register_cell_magic
+from pathlib import Path
+
+class FormatHandlerFactory:
+
+    handlers = {}
+    
+    @classmethod
+    def create(cls, format, *args, **kwargs):
+        return cls.handlers.get(format, "JSON")(*args, **kwargs)
+    
+    @classmethod
+    def register(cls, handlerClass):
+        cls.handlers[handlerClass.format] = handlerClass
+        return handlerClass
+
+class FormatHandler:
+    format = ""
+    
+    def printHeader(self, r, args):
+        for k, v in r.headers.items():
+            print("{}: {}".format(k, v))
+    
+    def handle(self, r, args):
+        pass
+
+@FormatHandlerFactory.register
+class JSONFormatHandler(FormatHandler):
+    format = "JSON"
+    
+    def printTime(self, r): 
+        print(f"Elapsed: {r.json()['statistics']['elapsed']} sec, read {r.json()['statistics']['rows_read']} rows, {r.json()['statistics']['bytes_read']} bytes.")
+    
+    def handle(self, r, args):
+        ret = ""
+        if args.verbose: self.printHeader(r, args)
+        if r.ok:
+            self.printTime(r)
+            ret = pd.DataFrame(r.json()['data'])     
+        else:
+            print("Bad Request: {}".format(r.text))
+        return ret
+    
+
+@FormatHandlerFactory.register
+class VerticalFormatHandler(FormatHandler):
+    format = "Vertical"
+        
+    def handle(self, r, args):
+        ret = ""
+        if args.verbose: self.printHeader(r, args)
+        if r.ok:
+            ret = Pretty(r.text)
+        else:
+            print("Bad Request: {}".format(r.text))
+        return ret
+        
+        
+@FormatHandlerFactory.register
+class PrettyFormatHandler(VerticalFormatHandler):
+    format = "Pretty"
+    
+    
+@FormatHandlerFactory.register
+class PrettyCompactFormatHandler(VerticalFormatHandler):
+    format = "PrettyCompact"
+    
 
 @register_cell_magic
 def ck(line, cell):
-
     parser = getParser()
     inArgs = line.strip().split(' ')
     if inArgs == ['']:
@@ -20,8 +85,6 @@ def ck(line, cell):
     query = sio.read()
 
     ret = queryClickHousePlayGround(query, url, args)
-    if not isinstance(ret, pd.DataFrame):
-        ret = Pretty(ret)
     return ret
 
 def queryClickHousePlayGround(query: str, url: str, args: argparse.Namespace):
@@ -29,24 +92,20 @@ def queryClickHousePlayGround(query: str, url: str, args: argparse.Namespace):
         'query':  query,
         'user': 'play',
         'password': '',
-        'default_format': "JSON"
+        'default_format': args.format
     }
     try: 
         r = requests.get(url, params=data)
     except Exception as e:
         print(e)
-    if r.ok:
-        ret = pd.DataFrame(r.json()['data'])
-        if args.plain:
-            if ret.shape != (1, 1):
-                print("--plain only supports results shape = (1, 1). current shape: {}".format(ret.shape))
-                sys.exit(1)
-            ret = str(ret.iloc[0,0])
-        print(f"Elapsed: {r.json()['statistics']['elapsed']} sec, read {r.json()['statistics']['rows_read']} rows, {r.json()['statistics']['bytes_read']} bytes.")
-    else:
-        print("Bad Request: {}".format(r.text))
-    for k, v in r.headers.items():
-        print("{}: {}".format(k, v))
+    
+    handler = FormatHandlerFactory.create(args.format)
+    ret = handler.handle(r, args)
+    if args.output is not None:
+        if args.format not in {"JSON"}:
+            ret.to_csv(args.outputFile, index=False)
+        else:
+            print("Only format JSON supports output!")
     return ret
 
 def load_ipython_extension(ipython):
@@ -59,7 +118,7 @@ def parseDataset(dataset: str) -> str:
 def getParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', dest='url', type=parseDataset, default=parseDataset('default'))
-    parser.add_argument('--plain', action='store_true', default=False)
+    parser.add_argument('--verbose', action='store_true', default=False)
+    parser.add_argument('--format', type=str, default='PrettyCompact', choices=list(FormatHandlerFactory.handlers.keys()))
+    parser.add_argument('--output', type=str)
     return parser
-
-load_ipython_extension(get_ipython())
