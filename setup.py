41ce428ee63e72d47755e674c4d7459b23e174ca
catwang01 <edwardelricwzx@gmail.com>
2023-06-11 05:43:47 +0100

    move version on

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="JupyterMagicCommands", # distribution package  name
-    version="0.0.2",
+    version="0.1.0",
     author="ed",
     author_email="edwardelricwzx@example.com",
     description="Some useful magic commands for juypter",
