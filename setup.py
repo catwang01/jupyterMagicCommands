01d06f1e0b9e239cd96899ee7320880731c5e52b
catwang01 <edwardelricwzx@gmail.com>
2023-08-12 13:32:34 +0100

    add drawio related

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="JupyterMagicCommands", # distribution package  name
-    version="0.1.2",
+    version="0.1.3",
     author="ed",
     author_email="edwardelricwzx@example.com",
     description="Some useful magic commands for juypter",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
-    install_requires=['ipython', 'plantuml', 'pandas', 'requests', "pytest", "docker", "openai"]
+    install_requires=['ipython', 'plantuml', 'pandas', 'requests', "pytest", "docker", "openai", "ipydrawio[all]"]
 )
