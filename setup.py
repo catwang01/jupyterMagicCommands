100004e7b6747f1792207f4cc68aaf8d417f2b67
catwang01 <edwardelricwzx@gmail.com>
2023-08-30 14:42:40 +0100

    update code

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ import setuptools
 
 setuptools.setup(
     name="JupyterMagicCommands", # distribution package  name
-    version="0.1.3",
+    version="0.1.4",
     author="ed",
     author_email="edwardelricwzx@example.com",
     description="Some useful magic commands for juypter",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
-    install_requires=['ipython', 'plantuml', 'pandas', 'requests', "pytest", "docker", "openai", "ipydrawio[all]"]
+    install_requires=['ipython', 
+                    'plantuml', 
+                    'pandas', 
+                    'requests', 
+                    "pytest",
+                    "docker", 
+                    "openai", 
+                    "ipydrawio[all]"]
 )
