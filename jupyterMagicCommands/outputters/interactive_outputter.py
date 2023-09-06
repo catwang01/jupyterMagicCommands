02f3786ffde99778e36408f6e0a149d5ffe42ff0
catwang01 <edwardelricwzx@gmail.com>
2023-09-06 17:08:47 +0100

    add button

--- jupyterMagicCommands/outputters/interactive_outputter.py
+++ jupyterMagicCommands/outputters/interactive_outputter.py
@@ -14,12 +14,22 @@ class InteractiveOutputter(AbstractOutputter):
         self.poll = self.ui.__enter__()
         self.out = widgets.Output()
         self.text = widgets.Text(placeholder='input')
+        self.sendEnterWidget = widgets.ToggleButtons(
+            options=['Send Enter', "Don't send enter"],
+            description='Send enter?:',
+            disabled=False,
+        )
         self.read_cb: AbstractOutputterReadCB = EmptyOutputterReadCB()
-        display(widgets.VBox([self.out, self.text]))
+        display(widgets.VBox([self.out, self.text, self.sendEnterWidget]))
 
     def register_read_callback(self, cb) -> None:
         self.read_cb = cb
-        self.text.on_submit(lambda widget: cb(widget.value))
+        def wrapped_cb(widget):
+            if self.sendEnterWidget.value == 'Send Enter':
+                cb(widget.value + '\n')
+            else:
+               cb(widget.value)
+        self.text.on_submit(wrapped_cb)
 
     def write(self, s):
         self.out.append_stdout(s)
