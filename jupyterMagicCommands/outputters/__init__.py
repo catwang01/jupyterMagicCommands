96cfdfdeff2c747e1e0f3f0fbf8bfbd522234523
catwang01 <edwardelricwzx@gmail.com>
2023-09-06 16:18:24 +0100

    remove asyIcinteractiveOutputter

--- jupyterMagicCommands/outputters/__init__.py
+++ jupyterMagicCommands/outputters/__init__.py
@@ -1,5 +1,3 @@
-from jupyterMagicCommands.outputters.async_interactive_outputter import \
-    AsyncInteractiveOutputter
 from jupyterMagicCommands.outputters.interactive_outputter import \
     InteractiveOutputter
 from jupyterMagicCommands.outputters.outputter import (AbstractOutputter,
