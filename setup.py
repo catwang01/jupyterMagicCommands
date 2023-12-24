9c132af913838970b96220558d54720e1decd751
catwang01 <edwardelricwzx@gmail.com>
2023-12-24 10:13:31 +0000

    format setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ import setuptools
 from tools import get_version
 
 setuptools.setup(
-    name="JupyterMagicCommands", # distribution package  name
+    name="JupyterMagicCommands",  # distribution package  name
     version=get_version(),
     author="ed",
     author_email="edwardelricwzx@example.com",
     description="Some useful magic commands for juypter",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
-    install_requires=['ipython', 
-                    'plantuml', 
-                    'jupyter_ui_poll',
-                    'pandas', 
-                    'requests', 
-                    "pytest",
-                    "docker", 
-                    "openai", 
-                    "ipydrawio[all]"]
+    install_requires=[
+        "ipython",
+        "plantuml",
+        "jupyter_ui_poll",
+        "pandas",
+        "requests",
+        "pytest",
+        "docker",
+        "openai",
+        "ipydrawio[all]",
+    ],
 )
