887d324db482c94f425926cf2c9d19315cc8437b
catwang01 <edwardelricwzx@gmail.com>
2024-07-20 10:31:17 +0000

    refactor a little bit

--- src/jupyterMagicCommands/filesystem/filesystem.py
+++ src/jupyterMagicCommands/filesystem/filesystem.py
@@ -53,6 +53,49 @@ class FileSystem(IFileSystem):
         else:
             raise Exception(f"Path '{path}' does not exist")
 
+    def _find_pid(self, actual_cmd_without_redirections: str) -> Optional[int]:
+        # we use the true pid
+        processes = psutil.process_iter(attrs=["pid", "name", "cmdline"])
+        for p in processes:
+            command = " ".join(p.cmdline())
+            self.logger.debug(f'The process {p} has the command {command}')
+            # the correctness is built on the assumption that the template file saving the cell content is only run by this class
+            if command and actual_cmd_without_redirections == command:
+                self.logger.info(f"Find a process: {p.info}")
+                return p.pid
+        return None
+
+    def _system_background(
+            self,
+            actual_cmd: str,
+            outFile: Optional[str] = None,
+            outVar: Optional[str] = None,
+            proc: Optional[str] = None,
+            delay: int = -1,
+        ) -> None:
+        # for background script, we use the built-in `%%script` magic to help
+        if outFile is None and outVar is None:
+            outFile = "/tmp/out.log"
+            print(f"WARNING: outFile is not set, the default output file is {outFile}")
+        actual_cmd_without_redirections = actual_cmd
+        actual_cmd += f'> "{outFile}" 2>&1'
+
+        # the process output is exported to the variable with name random_variable_name
+        random_variable_name = "outVar" + str(hash(outFile))
+        proc = proc or random_variable_name
+        self.shell.run_cell_magic("_script", f"bash --bg --proc {proc} --wait-after {delay}", actual_cmd)
+        self.shell.system(f'ps aux | grep -v grep | grep "{actual_cmd}"')
+        pid = self._find_pid(actual_cmd_without_redirections)
+        # if the true pic can't be obtained, use the parent pid
+        if pid is not None:
+            print(f"Run a subprocess with pid: {pid}. Output to '{outFile}'")
+        else:
+            self.logger.info(f'Not process with command {actual_cmd_without_redirections} is found. Use the the pid of the parent process')
+            child = self.shell.user_ns[proc]
+            pid = child.pid
+            print(f"Run a subprocess. Its parent process's pid is {pid}. Output to '{outFile}'")
+        self.shell.user_ns[proc] = pid
+
     def system(
         self,
         cmd: str,
@@ -79,37 +122,7 @@ class FileSystem(IFileSystem):
             self.logger.debug(f'Saved the content into {fp.name}, the actual command to run is {actual_cmd}')
 
         if background: 
-            # for background script, we use the built-in `%%script` magic to help
-            if outFile is None and outVar is None:
-                outFile = "/tmp/out.log"
-                print(f"WARNING: outFile is not set, the default output file is {outFile}")
-            actual_cmd_without_redirections = actual_cmd
-            actual_cmd += f'> "{outFile}" 2>&1'
-
-            # the process output is exported to the variable with name random_variable_name
-            random_variable_name = "outVar" + str(hash(outFile))
-            proc = proc or random_variable_name
-            self.shell.run_cell_magic("_script", f"bash --bg --proc {proc} --wait-after {delay}", actual_cmd)
-            self.shell.system(f'ps aux | grep -v grep | grep "{actual_cmd}"')
-            processes = psutil.process_iter(attrs=["pid", "name", "cmdline"])
-            pid = None
-            # we use the true pid
-            for p in processes:
-                command = " ".join(p.cmdline())
-                self.logger.debug(f'The process command is {command}')
-                # the correctness is built on the assumption that the template file saving the cell content is only run by this class
-                if command and actual_cmd_without_redirections == command:
-                    self.logger.info(f"Find a process: {p.info}")
-                    pid = p.pid
-                    print(f"Run a subprocess with pid: {pid}. Output to '{outFile}'")
-                    break
-            # if the true pic can't be obtained, use the parent pid
-            if pid is None:
-                self.logger.info(f'Not process with command {actual_cmd_without_redirections} is found. Use the the pid of the parent process')
-                child = self.shell.user_ns[proc]
-                pid = child.pid
-                print(f"Run a subprocess. It's parent process's pid is {pid}. Output to '{outFile}'")
-            self.shell.user_ns[proc] = pid
+            self._system_background(actual_cmd, outFile, outVar, proc, delay)
             return
 
         child = pexpect.spawn(actual_cmd)
