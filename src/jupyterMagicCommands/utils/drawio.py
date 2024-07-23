fc9b8aa2902e5e5125968388d709a606aac9a219
catwang01 <edwardelricwzx@gmail.com>
2024-07-23 08:14:10 +0000

    invoke drawio.export in drawio

--- src/jupyterMagicCommands/utils/drawio.py
+++ src/jupyterMagicCommands/utils/drawio.py
@@ -1,7 +1,11 @@
+import os
+import subprocess
+import tempfile
 from pathlib import Path
+from typing import List, Optional
 
 import nbformat
-from IPython.display import display
+from IPython.display import display, Image
 from lxml import etree
 
 MIMETYPE = "application/x-drawio"
@@ -26,11 +30,16 @@ def get_xmfile_to_show(xml, target_diagram):
             xml.remove(diagram)
     return etree.tostring(xml).decode()
 
+def list_diagram_names(xml) -> List[str]:
+   return [ diagram.attrib.get("name") for diagram in xml ]
 
-def get_target_diagram(xml, target_name):
+def normalize_name(s: str) -> str:
+    return s.lower()
+
+def get_target_diagram(xml, target_name: str):
     for diagram in xml:
         name = diagram.attrib.get("name")
-        if name == target_name:
+        if normalize_name(name) == normalize_name(target_name):
             return diagram
 
 
@@ -41,9 +50,26 @@ def get_xml(notebook):
     return xml
 
 
-def drawio_image(target_name, display_options=None):
+def drawio_image(target_name: str, display_options: Optional[dict]=None):
+    processed_target_name = normalize_name(target_name)
     notebook = get_current_notebook()
     xml = get_xml(notebook)
-    target_diagram = get_target_diagram(xml, target_name)
+    diagram_names = { normalize_name(name) for name in list_diagram_names(xml) }
+    if processed_target_name not in diagram_names:
+        print(f"Not a valid diagram '{processed_target_name}'. The valid diagram names are {', '.join(diagram_names)}")
+        return
+    target_diagram = get_target_diagram(xml, processed_target_name)
     xml_str = get_xmfile_to_show(xml, target_diagram)
-    display_drawio(xml_str, metadata={MIMETYPE: display_options or {}})
+    with tempfile.TemporaryDirectory() as tempdir:
+        xml_file_path = os.path.join(tempdir, 'test.xml')
+        png_file_path = os.path.join(tempdir, 'test.xml.png')
+        with open(xml_file_path, 'w') as f:
+            f.write(xml_str)
+        s = f"""docker run --rm \
+                -v {tempdir.name}:/files \
+                b1f6c1c4/draw.io-export png
+        """
+        ret = subprocess.call(s)
+        display(Image(filename=png_file_path))
+
+    # display_drawio(xml_str, metadata={MIMETYPE: display_options or {}})
