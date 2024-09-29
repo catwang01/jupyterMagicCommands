3c988c1d4a94a52afb6c8e02fe50b7acef51465f
catwang01 <edwardelricwzx@gmail.com>
2024-09-29 01:13:24 +0000

    adding cache to cs to reduce initialization time of the dotnet project

--- src/jupyterMagicCommands/extensions/cs_ext.py
+++ src/jupyterMagicCommands/extensions/cs_ext.py
@@ -1,10 +1,12 @@
 import argparse
 import logging
 import sys
-from pathlib import Path
-from tempfile import TemporaryDirectory
+from typing import List, Set, TypedDict
 
-from jupyterMagicCommands.utils.cmd import executeCmd
+from jupyterMagicCommands.utils.cs import (CSCodeProjectCacheManager,
+                                           CSCodeRunner, CSCodeRunnerOptions,
+                                           DotnetCli, PackageInfo,
+                                           processPackageInfo)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -12,101 +14,65 @@ logger.setLevel(logging.INFO)
 streamhandler = logging.StreamHandler()
 streamhandler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter("%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s"
+)
 streamhandler.setFormatter(formatter)
 
 logger.addHandler(streamhandler)
 
+dotnetCli = DotnetCli(logger=logger)
+cacheManager = CSCodeProjectCacheManager(dotnetCli, logger=logger)
+runner = CSCodeRunner(cacheManager, dotnetCli, logger=logger)
 
-def analyzeCode(code):
-    usingLines = []
-    bodyLines = []
-    status = 0
-    for line in code.split('\n'):
-        if status == 0 or status == 1:
-            if line.startswith("using"):
-                if status == 0: status = 1
-                usingLines.append(line)
-            elif line.strip() == '': 
-                pass
-            else:
-                status = 2
-                bodyLines.append(line)
-        else:
-            bodyLines.append(line)
-            
-    return {'using': '\n'.join(usingLines), 'body': "\n".join(bodyLines)}
-
-def completeCode(code, args):
-    # TODO: update here 
-    if "Main" in code:
-        return code
-    else:
-        analyzedCode = analyzeCode(code)
-        skelton = """
-        {using}
-        namespace DemoApplication {{
-            public class Program {{
-                public static void Main() {{
-                    {body}
-                }}
-            }}
-        }}
-        """
-        code = skelton.format(**analyzedCode)
-    return code
-
-def runCsharp(cell, args):
-    # since C#9, there is no need to have a Main
-    # code = completeCode(cell, args)
-    code = cell
-    logger.debug(code)
-    verbose = args.verbose
-    with TemporaryDirectory() as tmpDir:
-        executeCmd(f"dotnet new console", cwd=tmpDir, verbose=verbose)
-        tmpFile = Path(tmpDir) / 'Program.cs'
-        tmpFile.write_text(code)
-        for packageInfo in args.packageInfos:
-            if "version" in packageInfo:
-                executeCmd(f"dotnet add package {packageInfo['name']} --version {packageInfo['version']}", cwd=tmpDir, verbose=verbose)
-            else:
-                executeCmd(f"dotnet add package {packageInfo['name']}", cwd=tmpDir, verbose=verbose)
-        executeCmd("dotnet run", cwd=tmpDir, verbose=True)
-                
 def transformLogLevel(s):
-    level = {
-        'INFO': logging.INFO,
-        "DEBUG": logging.DEBUG
-    }.get(s.upper())
+    level = {"INFO": logging.INFO, "DEBUG": logging.DEBUG}.get(s.upper())
     if level is None:
         print(f"Unsupported level: {s}")
         sys.exit(1)
     return level
 
-def processPackageInfo(s):
-    ret = {}
-    infos = s.split("@")
-    if len(infos) == 1:
-        ret['name'] = infos[0]
-    elif len(infos) == 2:
-        ret['name'] = infos[0]
-        ret['version'] = infos[1]
-    else:
-        raise Exception(f"Not a valid package info: {s}. Package info should be <package-name> or <package-name>@<version>")
-    return ret
-
 def cs(line, cell):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--logLevel', default='INFO', type=transformLogLevel)
-    parser.add_argument('--addPackage', dest="packageInfos", type=processPackageInfo , action="append" , default=[], help="package information formatted as <packagename>@<version>")
-    parser.add_argument('--verbose', action="store_true", default=False)
-    if line.strip() != '':
-        args = parser.parse_args(line.strip(' ').split(' '))
+    parser.add_argument("--logLevel", default="INFO", type=transformLogLevel)
+    parser.add_argument(
+        "--addPackage",
+        dest="packages",
+        type=processPackageInfo,
+        action="append",
+        default=[],
+        help="package information formatted as <packagename>@<version>",
+    )
+    parser.add_argument(
+        "--disable-cache",
+        dest="cache",
+        default=True,
+        action="store_false",
+        help="Whether disable caching for creating packages",
+    )
+    parser.add_argument("--verbose", action="store_true", default=False)
+    if line.strip() != "":
+        args = parser.parse_args(line.strip(" ").split(" "))
     else:
         args = parser.parse_args([])
+
+    specifiedPackages: Set[str] = set()
+    for package in args.packages:
+        name = package["name"]
+        if name in specifiedPackages:
+            print(f"Can't specify the same package multiple times: {name}")
+            sys.exit(1)
+        specifiedPackages.add(name)
+
     logger.setLevel(args.logLevel)
     logger.debug("args: %s", args)
-    runCsharp(cell, args)
-    
+
+    dotnetCli.verbose = args.verbose
+    runnerOptions: CSCodeRunnerOptions = vars(args)
+    runner.options = runnerOptions
+    logger.debug("runnerOptions: %s", runnerOptions)
+    runner.runCsharp(cell)
+    cacheManager.saveCache()
+
 def load_ipython_extension(ipython):
-    ipython.register_magic_function(cs, 'cell')
+    ipython.register_magic_function(cs, "cell")
---
+++ src/jupyterMagicCommands/utils/cs.py
@@ -0,0 +1,291 @@
+from dataclasses import dataclass
+import dataclasses
+from enum import Enum
+from hashlib import md5
+import json
+from pprint import pformat
+from logging import Logger
+import shutil
+from typing import Dict, List, Optional, Tuple, TypedDict
+from typing_extensions import NotRequired, Required
+from jupyterMagicCommands.utils.cmd import executeCmd
+
+
+from pathlib import Path
+from tempfile import mkdtemp
+
+from jupyterMagicCommands.utils.log import NULL_LOGGER
+
+
+class PackageInfo(TypedDict):
+    name: Required[str]
+    version: NotRequired[str]
+
+@dataclass
+class CacheInfoItem:
+    hitCount: int
+    packages: Dict[str, PackageInfo]
+
+CacheInfo = Dict[str, CacheInfoItem]
+
+class CacheStatus(Enum):
+    MISS = 0  # Not used at this time
+    PARTIAL_HIT = 1
+    FULL_HIT = 2
+
+
+class IDotnetCli:
+
+    def addPackage(self, package: PackageInfo, cwd: Optional[str] = None) -> None:
+        pass
+
+    def new(self, projectType: str, projectName: str, cwd: Optional[str] = None) -> None:
+        pass
+
+    def run(self, cwd: Optional[str] = None) -> None:
+        pass
+
+    @property
+    def cwd(self) -> str:
+        pass
+
+    @cwd.setter
+    def cwd(self, value: str) -> None:
+        pass
+
+
+class DotnetCli(IDotnetCli):
+
+    def __init__(self, cwd: str = ".", verbose: bool=False, logger: Logger=NULL_LOGGER) -> None:
+        self._cwd = cwd
+        self.logger = logger
+        self.verbose = verbose
+
+    @property
+    def cwd(self) -> str:
+        return self._cwd
+
+    @cwd.setter
+    def cwd(self, value):
+        self._cwd = value
+
+    def addPackage(self, package: PackageInfo, cwd: Optional[str] = None) -> None:
+        if "version" in package:
+            executeCmd(
+                f"dotnet add package {package['name']} --version {package['version']}",
+                cwd=cwd or self.cwd,
+                verbose=self.verbose,
+            )
+        else:
+            executeCmd(
+                f"dotnet add package {package['name']}",
+                cwd=cwd or self.cwd,
+                verbose=self.verbose,
+            )
+
+    def run(self, cwd: Optional[str] = None) -> None:
+        # always verbose for `dotnet run`, because it will print the output of the program
+        executeCmd("dotnet run", cwd=cwd or self.cwd, verbose=True, backend="popen")
+
+    def new(self, projectType: str, projectName: str, cwd: Optional[str] = None) -> None:
+        executeCmd(
+            f"dotnet new {projectType} -o {projectName}",
+            cwd=cwd or self.cwd,
+            verbose=self.verbose,
+        )
+
+
+class CSCodeProjectCacheManager:
+
+    cacheRoot: Path
+    cacheInfoFile: Path
+    logger: Logger
+    dotnetCli: IDotnetCli
+    cacheInfo: CacheInfo
+    EMPTY_CACHE_KEY = "__empty__"
+
+    def __init__(self, dotnetCli: IDotnetCli, cacheRoot: Optional[Path] = None, logger: Logger = NULL_LOGGER) -> None:
+        self.dotnetCli = dotnetCli
+        self.logger = logger
+        self.cacheRoot = cacheRoot or Path("/tmp/jmc/cs")
+        if not self.cacheRoot.exists():
+            self.cacheRoot.mkdir(parents=True)
+        self.cacheInfoFile = self.cacheRoot / "cache.json"
+
+        if self.cacheInfoFile.exists():
+            try:
+                self.cacheInfo = json.loads(self.cacheInfoFile.read_text(), cls=self.EnhancedJSONDecoder)
+            except Exception as e:
+                raise Exception(f"Failed to load cache info") from e
+        else:
+            self.cacheInfo = {}
+            cache_path = self.cacheRoot / self.EMPTY_CACHE_KEY
+            if not cache_path.exists():
+                self.dotnetCli.new(
+                    "console", projectName=self.EMPTY_CACHE_KEY, cwd=str(self.cacheRoot)
+                )
+            self._addCacheItem(self.EMPTY_CACHE_KEY, [])
+
+    def tryToLoadFromCache(
+        self, directory: str, packages
+    ) -> Tuple[CacheStatus, List[PackageInfo]]:
+
+        if len(packages) == 0:
+            self._copyFromCache(self.EMPTY_CACHE_KEY, directory)
+            return CacheStatus.FULL_HIT, []
+        else:
+            shortestMissingPackages = packages
+            hittedCacheDir = self.EMPTY_CACHE_KEY
+            for cacheDir, cacheInfoItem in self.cacheInfo.items():
+                hittedPackages, missingPackageInfo = self.getHittedPackages(
+                    cacheInfoItem.packages, packages
+                )
+                if shortestMissingPackages is None or len(missingPackageInfo) < len(
+                    shortestMissingPackages
+                ):
+                    shortestMissingPackages = missingPackageInfo
+                    hittedCacheDir = cacheDir
+            self._copyFromCache(hittedCacheDir, directory)
+            cacheStatus = CacheStatus.PARTIAL_HIT if len(shortestMissingPackages) > 0 \
+                                                  else CacheStatus.FULL_HIT
+            return cacheStatus, shortestMissingPackages
+
+    def _addToCache(self, directory: str, packages: List[PackageInfo]) -> None:
+        cacheKey = md5(json.dumps(packages).encode()).hexdigest()
+        shutil.copytree(directory, self.cacheRoot / cacheKey, dirs_exist_ok=True)
+        self._addCacheItem(cacheKey, packages)
+
+    def _copyFromCache(self, cacheKey: str, directory: str) -> None:
+        shutil.copytree(
+            self.cacheRoot / cacheKey, directory, dirs_exist_ok=True
+        )
+        self.cacheInfo[cacheKey].hitCount += 1
+        self.logger.debug("hit cache to %s", cacheKey)
+
+    def _addCacheItem(self, cacheKey: str, packages: List[PackageInfo]) -> None:
+        self.cacheInfo[cacheKey] = CacheInfoItem(
+            hitCount=0,
+            packages={package["name"]: package for package in packages}
+        )
+
+    def getHittedPackages(
+        self, packageInfosA: Dict[str, PackageInfo], packages: List[PackageInfo]
+    ) -> Tuple[List[PackageInfo], List[PackageInfo]]:
+        hittedPackages: List[PackageInfo] = []
+        missingPackages: List[PackageInfo] = []
+        for packageInfo in packages:
+            packageName = packageInfo.get("name")
+            if packageName is not None:  # sanity check
+                possiblePackageInfo = packageInfosA.get(packageName)
+                if possiblePackageInfo:
+                    target_version = packageInfo.get("version")
+                    if (
+                        target_version is None
+                        or target_version == possiblePackageInfo.get("version")
+                    ):
+                        hittedPackages.append(packageInfo)
+                        continue
+            missingPackages.append(packageInfo)
+        return hittedPackages, missingPackages
+
+    class EnhancedJSONEncoder(json.JSONEncoder):
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
+    
+    class EnhancedJSONDecoder(json.JSONDecoder):
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(object_hook=self.object_hook, *args, **kwargs)
+
+        def object_hook(self, obj):
+            if {"hitCount", "packages"} == set(obj.keys()):
+                return CacheInfoItem(**obj)
+            return obj
+
+    def saveCache(self) -> None:
+        self.logger.debug("Saving cache info into %s", self.cacheInfoFile)
+        self.logger.debug(pformat(self.cacheInfo))
+        with self.cacheInfoFile.open("w") as f:
+            json.dump(self.cacheInfo, f, cls=self.EnhancedJSONEncoder)
+
+class CSCodeRunnerOptions(TypedDict):
+    cache: bool
+    packages: List[PackageInfo]
+
+
+DEFAULT_CS_CODE_RUNNER_OPTIONS: CSCodeRunnerOptions = {
+    "cache": True,
+    "packages": [],
+}
+
+class CSCodeRunner:
+
+    dotnetCli: IDotnetCli
+
+    def __init__(
+        self,
+        cacheManager: CSCodeProjectCacheManager,
+        dotnetCli: IDotnetCli,
+        options: CSCodeRunnerOptions=DEFAULT_CS_CODE_RUNNER_OPTIONS,
+        logger: Logger = NULL_LOGGER,
+    ):
+        self.options = options
+        self.cacheManager = cacheManager
+        self.dotnetCli = dotnetCli
+        self.logger = logger
+    
+    @property
+    def cache(self) -> bool:
+        return self.options.get("cache", True)
+    
+    @property
+    def packages(self) -> List[PackageInfo]:
+        return self.options.get("packages", [])
+
+    def generateProject(self, directory: str) -> None:
+        cacheStatus = None
+        if not self.cache:
+            missingPackages = self.packages
+        else:
+            cacheStatus, missingPackages = self.cacheManager.tryToLoadFromCache(
+                directory, self.packages
+            )
+        for package in missingPackages:
+            self.dotnetCli.addPackage(package)
+        if self.cache and cacheStatus != CacheStatus.FULL_HIT:
+            self.cacheManager._addToCache(directory, self.packages)
+
+    def runCsharp(self, cell, directory: Optional[str] = None) -> None:
+        # since C#9, there is no need to have a Main
+        # code = completeCode(cell, args)
+        code = cell
+        self.logger.debug(code)
+        targetDirectory: Path
+        if directory is None:
+            targetDirectory = Path(mkdtemp())
+        else:
+            targetDirectory = Path(directory)
+        self.dotnetCli.cwd = str(targetDirectory)
+        self.logger.debug('Generating project in %s', targetDirectory)
+        self.generateProject(str(targetDirectory))
+        tmpFile = targetDirectory / "Program.cs"
+        tmpFile.write_text(code)
+        self.logger.debug('Running code in %s', targetDirectory)
+        self.dotnetCli.run()
+
+
+def processPackageInfo(s: str) -> PackageInfo:
+    ret: PackageInfo = {}  # type: ignore
+    infos = s.split("@")
+    if len(infos) == 1:
+        ret["name"] = infos[0]
+    elif len(infos) == 2:
+        ret["name"] = infos[0]
+        ret["version"] = infos[1]
+    else:
+        raise Exception(
+            f"Not a valid package info: {s}. Package info should be <package-name> or <package-name>@<version>"
+        )
+    return ret
