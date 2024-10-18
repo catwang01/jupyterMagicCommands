cda307aeeee5b8f67c6ff87c2bb3c1772b9788e9
catwang01 <edwardelricwzx@gmail.com>
2024-10-18 15:33:26 +0000

    merge

--- src/jupyterMagicCommands/utils/cmd.py
+++ src/jupyterMagicCommands/utils/cmd.py
@@ -2,14 +2,14 @@ import os
 import logging
 from IPython import get_ipython
 import subprocess
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
 from pathlib import Path
 import shlex
 
 logger = logging.getLogger(__name__)
 
 def _run_command(cmd):
-    with Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as p:
+    with Popen(shlex.split(cmd), stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True) as p:
         while True:
             line = p.stdout.readline()
             if not line:
@@ -21,16 +21,19 @@ def _run_command(cmd):
 def executeCmd(cmd, cwd='.', verbose=True, backend=None):
     currentDir = Path.cwd()
     backend = backend or "jmc"
+    backend = backend.lower()
     os.chdir(cwd)
+    ret = None
     try:
         # easy case, just use subprocess.run
         if not verbose: 
             ret = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         elif backend == "ipython":
             get_ipython().system(cmd)
-        elif backend == "jmc":
+        elif backend in { "popen", "jmc" }:
             _run_command(cmd)
         else:
-            raise Exception(f"Invalid backend: {backend}")
+            raise ValueError(f"Unsupported backend: {backend}")
     finally:
         os.chdir(currentDir)
+    return ret
